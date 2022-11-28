0be0b09e83dc4bf29ae5ca51b1ae06a568a1731e
Alexie (Boyong) Madolid <maex@tuta.io>
2022-11-28 17:16:58 +0800

    [JSORC-LOGGER]: Add some info logs on automated process of jsorc

--- jaseci_core/jaseci/svc/meta.py
+++ jaseci_core/jaseci/svc/meta.py
@@ -1,5 +1,5 @@
 import signal
-
+from jaseci.utils.utils import logger
 from jaseci.svc import (
     CommonService,
     JsOrc,
@@ -32,12 +32,17 @@ class MetaService(CommonService, MetaProperties):
         if self.run_svcs:
             self.app.build()
             if self.is_automated():
+                logger.info("JsOrc is automated. Pushing interval check alarm...")
                 self.push_interval(1)
+            else:
+                logger.info("JsOrc is not automated.")
 
     def push_interval(self, interval):
         if self.running_interval == 0:
             self.running_interval += 1
             signal.alarm(interval)
+        else:
+            logger.info("Reusing current running interval...")
 
     ###################################################
     #                    SERVICES                     #
@@ -134,6 +139,9 @@ def interval_check(signum, frame):
     meta = MetaService()
     if meta.is_automated():
         meta.app.interval_check()
+        logger.info(
+            f"Backing off for {meta.app.backoff_interval} seconds before the next interval check..."
+        )
         meta.push_interval(meta.app.backoff_interval)
     meta.running_interval -= 1
 
