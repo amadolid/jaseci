7861da2097401e5f8c8c196d1c93ccc891189133
Alexie (Boyong) Madolid <maex@tuta.io>
2023-01-09 16:55:47 +0800

    [STRIPE-SERVICE]: Initial implementation

---
+++ jaseci_core/jaseci/svc/stripe/__init__.py
@@ -0,0 +1,7 @@
+from .config import STRIPE_ERR_MSG
+from .stripe import StripeService
+
+__all__ = [
+    "STRIPE_ERR_MSG",
+    "StripeService",
+]
---
+++ jaseci_core/jaseci/svc/stripe/config.py
@@ -0,0 +1,3 @@
+STRIPE_ERR_MSG = "Stripe service is disabled or not yet configured!"
+
+STRIPE_CONFIG = {"enabled": False, "api_key": None, "walker": "stripe"}
