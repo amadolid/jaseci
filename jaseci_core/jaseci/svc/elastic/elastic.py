fbcf6f22ce7f31c1e7c55263f288fc565e27fcdb
Alexie (Boyong) Madolid <maex@tuta.io>
2023-01-12 19:46:28 +0800

    temporary

--- jaseci_core/jaseci/svc/elastic/elastic.py
+++ jaseci_core/jaseci/svc/elastic/elastic.py
@@ -41,35 +41,35 @@ class Elastic:
     def _post(self, url: str, json: dict):
         return post(f"{self.url}/{url}", json=json, headers=self.headers).json()
 
-    def post(self, url: str, body: dict, index: str):
-        return self._post(f"{index or self.common_index}{url}", body)
+    def post(self, url: str, body: dict, index: str, suffix: str = ""):
+        return self._post(f"{index or self.common_index}{suffix}{url}", body)
 
-    def post_act(self, url: str, body: dict):
-        return self.post(url, body, self.activity_index)
+    def post_act(self, url: str, body: dict, suffix: str = ""):
+        return self.post(url, body, self.activity_index, suffix)
 
-    def get(self, url: str, body: dict, index: str):
-        return self._get(f"{index or self.common_index}{url}", body)
+    def get(self, url: str, body: dict, index: str, suffix: str = ""):
+        return self._get(f"{index or self.common_index}{suffix}{url}", body)
 
-    def get_act(self, url: str, body: dict):
-        return self.get(url, body, self.activity_index)
+    def get_act(self, url: str, body: dict, suffix: str = ""):
+        return self.get(url, body, self.activity_index, suffix)
 
-    def doc(self, log: dict, query: str, index: str):
-        return self._post(f"{index or self.common_index}/_doc?{query}", log)
+    def doc(self, log: dict, query: str, index: str, suffix: str = ""):
+        return self.post(f"/_doc?{query}", log, index, suffix)
 
-    def doc_activity(self, log: dict, query: str):
-        return self.doc(log, query, self.activity_index)
+    def doc_activity(self, log: dict, query: str, suffix: str = ""):
+        return self.doc(log, query, self.activity_index, suffix)
 
-    def search(self, body: dict, query: str, index: str):
-        return self._get(f"{index or self.common_index}/_search?{query}", body)
+    def search(self, body: dict, query: str, index: str, suffix: str = ""):
+        return self.get(f"/_search?{query}", body, index, suffix)
 
-    def search_activity(self, body: dict, query: str):
-        return self.search(body, query, self.activity_index)
+    def search_activity(self, body: dict, query: str, suffix: str = ""):
+        return self.search(body, query, self.activity_index, suffix)
 
-    def mapping(self, query: str, index: str):
-        return self._get(f"{index or self.common_index}/_mapping?{query}")
+    def mapping(self, query: str, index: str, suffix: str = ""):
+        return self.get(f"/_mapping?{query}", index, suffix)
 
-    def mapping_activity(self, query: str):
-        return self.mapping(query, self.activity_index)
+    def mapping_activity(self, query: str, suffix: str = ""):
+        return self.mapping(query, self.activity_index, suffix)
 
     def aliases(self, query: str = "pretty=true"):
         return self._get(f"/_aliases?{query}")
